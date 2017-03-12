dae07a27c6c977e2cb0a66953608abb69fb0639e
giveme5 <286674783@qq.com>
2017-03-12 11:08:49 +0800

    day1:安装company，F2设置我打开init.el，关闭toolbar和scrollbar，设置光标为竖线

--- init.el
+++ init.el
@@ -1,20 +1,46 @@
 ;;这是手动设置的，下面是通过custom界面设置的
-;;(add-to-list 'default-frame-alist
-;;	     '(font . "Menlo-16"))
+(add-to-list 'default-frame-alist
+	     '(font . "Menlo-16"))
 
 
+
+;; ----------------------------
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+;;(message package-archives) 这个时候这个变量还是void
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 (package-initialize)
 
+
+
+;;day1 --------------------
+(tool-bar-mode -1)
+;(scroll-all-mode -1)
+(electric-indent-mode -1)
+(setq inhibit-splash-screen t)
+(global-linum-mode t)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+(global-company-mode t)
+(setq cursor-type 'bar)
+
+
+
+;;-------------下面是通过M-x customize自动配置的
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-linum-mode t))
+ '(global-linum-mode t)
+ '(package-selected-packages (quote (company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,5 +48,3 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Source Code Pro")))))
 
-(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
