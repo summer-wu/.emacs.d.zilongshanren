75a14e8593e7b9bfdafcf071d5dcb8f64e3b58db
giveme5 <286674783@qq.com>
2017-03-14 13:19:53 +0800

    添加C-c p f执行counsel-git

--- lisp/custom.el
+++ lisp/custom.el
@@ -15,4 +15,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js2-external-variable ((t (:foreground "dark gray")))))
-
--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -13,4 +13,6 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
+(global-set-key (kbd "C-c p f") 'counsel-git)
+
 (provide 'init-keybindings)
